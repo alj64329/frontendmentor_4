bf1c45444e02623d5d4d6e273abc807adf1ae2a5
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-08-05 15:38:45 -0700

    Done assingment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 function toggleMenu(){
     const menu = document.querySelector(".hamburger-links")
-    const icon = document.querySelector(".hamburger-menu")
+    const icon = document.querySelector(".hamburger-icon")
+    const image = document.querySelector(".close-open-icon")
 
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+    if(menu.classList.contains("open")){
+        menu.classList.remove("open")
+        image.src = "/images/icon-hamburger.svg"
+    }else{
+        menu.classList.add("open")
+        image.src = "/images/icon-close.svg"
+        image.style.zIndex = "50"
+        image.style.position = "relative"
+
+    }
 }
+
